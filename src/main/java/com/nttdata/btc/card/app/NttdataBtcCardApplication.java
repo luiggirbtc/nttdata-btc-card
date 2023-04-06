91040b9fb01b28397720dbc9d3adc273c849e95c
brayanlrs28 <riverasantiagobrayan@gmail.com>
2023-04-05 19:03:49 -0500

    update enable discovery

--- src/main/java/com/nttdata/btc/card/app/NttdataBtcCardApplication.java
+++ src/main/java/com/nttdata/btc/card/app/NttdataBtcCardApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.btc.card.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NttdataBtcCardApplication {
 
 	public static void main(String[] args) {
